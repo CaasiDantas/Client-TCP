6024086c81678f15d10f54e2827f26fcf5d547bf
Isaac Dantas <153383825+CaasiDantas@users.noreply.github.com>
2024-01-04 17:58:28 -0300

    Add files via upload

---
+++ Client_TCP.py
@@ -0,0 +1,20 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip = input("Digite o IP para a conexão: ")
+
+porta = int(input("Digite a porta para a conexão: "))
+
+mensagem_1 = input("Digite a mensagem para o servidor TCP: ")
+
+mensagem1_bytes = mensagem_1.encode("utf-8")
+
+try:
+    client.connect((ip, porta))
+    client.send(mensagem1_bytes)
+    pacotes_recebidos = client.recv(1024).decode()
+    print(pacotes_recebidos)
+except Exception as error:
+    print("Houve um erro ao fazer a conexão")
+    print(error)
