4e4d65affbec5fd512e5051118206d085ebf7f87
Isaac Dantas <153383825+CaasiDantas@users.noreply.github.com>
2024-08-26 14:06:09 -0300

    Client_TCP.py

--- Client_TCP.py
+++ Client_TCP.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -6,23 +7,25 @@ ip = input("Digite o IP para a conexão: ")
 
 porta = int(input("Digite a porta para a conexão: "))
 
+os.system("cls" if os.name == "nt" else "clear")
+
 try:
     
     client.connect((ip, porta))
 
     while True:
 
-        mensagem = input("Mensagem do Cliente: ") + "\n"
+        mensagem = input("Mensagem do Cliente: ")
         client.send(mensagem.encode()) 
         
-        if mensagem == "tchau\n" or "Tchau\n":
+        if mensagem.lower().strip() == "tchau":
             print("Fim da conexão")
             break
-        
+
         pacotes_recebidos = client.recv(100000).decode()
         print(f"Mensagem do Servidor: {pacotes_recebidos}")
         
-        if pacotes_recebidos == "tchau\n" or "Tchau\n":
+        if pacotes_recebidos.lower().strip() == "tchau":
             print("Fim da conexão")
             break
     client.close()
@@ -30,3 +33,4 @@ try:
 except Exception as error:
     print("Houve um erro ao fazer a conexão")
     print(error)
+    
